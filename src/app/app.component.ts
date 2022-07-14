119463f229ee9af9825adfabe2eda3431d19c360
Corey Franco <cfranco@curtze.com>
2022-07-14 09:06:16 -0400

    Fixed full-screen background color issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,37 @@
-import { Component, HostListener } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Renderer2, ViewEncapsulation } from '@angular/core';
 import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'wordclock2';
   backgroundColor = '#000';
   fontColor = '#fff';
 
-  constructor() { }
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
+
+  ngAfterViewInit(): void {
+    this.renderer.setStyle(this.el.nativeElement.ownerDocument.body, 'backgroundColor', this.backgroundColor);
+  }
 
   @HostListener('window:wallpaperPropertyListener', ['$event'])
   applyUserProperties(properties: any) {
-  if (properties.backgroundColor) {
-    // Convert the custom color to 0 - 255 range for CSS usage
-    var customColor = properties.backgroundColor.value.split(' ');
-    customColor = customColor.map(c => {return Math.ceil(c * 255);});
-    this.backgroundColor = 'rgb(' + customColor + ')';
+    // TO DO: Create service to get and set these values on each component
+    if (properties.backgroundColor) {
+      // Convert the custom color to 0 - 255 range for CSS usage
+      var customColor = properties.backgroundColor.value.split(' ');
+      customColor = customColor.map(c => { return Math.ceil(c * 255); });
+      this.backgroundColor = 'rgb(' + customColor + ')';
+    }
+    if (properties.fontColor) {
+      // Convert the custom color to 0 - 255 range for CSS usage
+      var customColor = properties.fontColor.value.split(' ');
+      customColor = customColor.map(c => { return Math.ceil(c * 255); });
+      this.fontColor = 'rgb(' + customColor + ')';
+    }
   }
-  if (properties.fontColor) {
-    // Convert the custom color to 0 - 255 range for CSS usage
-    var customColor = properties.fontColor.value.split(' ');
-    customColor = customColor.map(c => {return Math.ceil(c * 255);});
-    this.fontColor = 'rgb(' + customColor + ')';
-  }
-}
 }
