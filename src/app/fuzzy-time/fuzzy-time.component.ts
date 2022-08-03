cc31e20a1e53bd5c1a6f81ed4bbfd0faeeb48ea6
Corey Franco <coreyfranco2@gmail.com>
2022-08-03 13:13:06 -0400

    Animations work again

--- src/app/fuzzy-time/fuzzy-time.component.ts
+++ src/app/fuzzy-time/fuzzy-time.component.ts
@@ -3,25 +3,28 @@ import { WordTime } from '../models/word-time.model';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { SettingsService } from '../services/settings.service';
 
-export const fadeInOutTimeout = 2000;
-export const fadeInOut = trigger('fadeInOut', [
-  transition('* => *', [style({ opacity: '0' }), animate(fadeInOutTimeout)])
-]);
-
 @Component({
   selector: 'app-fuzzy-time',
   templateUrl: './fuzzy-time.component.html',
   styleUrls: ['./fuzzy-time.component.scss'],
-  animations: [fadeInOut]
+  animations: [  trigger("fadeAnimation", [
+    transition("false=>true", [
+      style({ opacity: 0 }), //At begin animation, opacity=0
+      animate("1000ms", style({ opacity: 1 }))  //the animation makes opacity=0 to opacity=1
+    ])
+  ])]
 })
 
 export class FuzzyTimeComponent implements OnInit {
   public wordTime: WordTime = new WordTime();
+  toggle:boolean=false;
+
   constructor(public settingsService: SettingsService) {
     setInterval(() => {
       var now = new Date();
       if (now.getMinutes() != this.wordTime.currentTime.getMinutes()) {
         this.wordTime = new WordTime();
+        this.toggle= true;
       }
     }, 1000);
   }
